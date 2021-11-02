a54ee41df0a99bd3642790fd2f4b089dfbd2b262
unknown <elaniobros@gmail.com>
2021-11-01 21:10:37 -0300

    Ajuste e adição da lib de seleção mutiplas js

--- resources/views/usuario/gerenciar/edit_filme.blade.php
+++ resources/views/usuario/gerenciar/edit_filme.blade.php
@@ -3,23 +3,28 @@
     Filme Adiconar/Editar
 @endsection
 @section('style')
+    <link rel="stylesheet" href="{{ URL::asset('libs/bootstrap-select/dist/css/bootstrap-select.css') }}">
     <style>
-        .h-100 {
+        .h-80{
             height: 80vh !important;
         }
-
         .add-filme{
             width:3.5em;
-            height:100%;
+            height:5em;
             font-size:2em;
             background-color:#FFFFFFCC;
+        }
+        .add-filme-active{
             color: black;
         }
-        .add-filme:hover{
+        .add-filme-disable{
+            color: #a1a1a1FF;
+        }
+        .add-filme-active:hover{
             cursor: pointer;
             background-color:#FFFFFFEE;
         }
-        .add-filme:active{
+        .add-filme-active:active{
             background-color:#FFFFFFCC;
         }
 
@@ -32,29 +37,36 @@
 @endsection
 @section('content')
     @include('content.nav')
-    <div class="d-flex justify-content-center align-items-center h-100">
+    <div class="d-flex justify-content-center align-items-center h-80">
         <form class="bg-light p-5 rounded w-75">
             <div class="d-flex flex-column">
                 <div class="form-group d-flex flex-row w-100">
-                    <div class="mr-2">
-                        <div class="card add-filme d-flex justify-content-center align-items-center">
+                    <div class="mr-2 d-flex flex-column">
+                        <div class="card add-filme add-filme-active d-flex justify-content-center align-items-center mb-2">
                             <i class="fas fa-plus mb-2"></i>
                         </div>
-                        <input type="file" name="img" style="display:none" id="img" accept=".png, .jpge, .jpg">
-                        <select name="" id="">
-                            <option value="">Capa de um Arquivo</option>
-                            <option value="">Capa IMDB</option>
+                        <input type="file" name="img" style="display:none" id="case-filme" accept=".png, .jpge, .jpg">
+                        <select name="type_file" class="form-control">
+                            <option value="file" selected>Capa Arquivo</option>
+                            <option value="imdb">Capa IMDB</option>
                         </select>
                     </div>
-                    <div class="d-flex flex-column">
-                        <div class="form-group w-100">
+                    <div class="d-flex flex-column w-75">
+                        <div class="form-group">
                             <input type="text" class="form-control" name="title" placeholder="Titulo Filme">
                         </div>
                         <div class="form-group">
                             <input type="text" class="form-control" name="cod" placeholder="Código IMDB">
                         </div>
                         <div class="form-group">
-                            <input type="text" class="form-control" name="categoria" placeholder="Categorias">
+                            <select name="categoria" class="selectpicker w-100" title="Selecione as categorias" data-live-search="true" data-size="4" multiple>
+                                <option value="ola">Ola</option>
+                                <option value='tudo bem'>Tudo bem</option>
+                                <option value='massa'>Massa</option>
+                                <option>Ola1</option>
+                                <option>Tudo bem2</option>
+                                <option>Massa</option>
+                            </select>
                         </div>
                     </div>
                 </div>
@@ -67,9 +79,26 @@
     </div>
 @endsection
 @section('script')
+    <script src='{{ URL::asset('libs/bootstrap-select/dist/js/bootstrap-select.js') }}'></script>
     <script>
+        $('.selectpicker').selectpicker();
+        // $('.selectpicker').selectpicker('val', ['Ola','Tudo bem']);
         $('form .add-filme').click(function() {
-            $('#img').trigger('click');
+            $('#case-filme').trigger('click');
         });
+        $('select').change(function(){
+            if(this.value=='imdb'){
+                $('#case-filme').attr('disabled','')
+                $('#case-filme').removeAttr('name')
+                $('.add-filme').removeClass('add-filme-active')
+                $('.add-filme').addClass('add-filme-disable')
+            }
+            else{
+                $('#case-filme').removeAttr('disabled')
+                $('#case-filme').attr('name','img')
+                $('.add-filme').removeClass('add-filme-disable')
+                $('.add-filme').addClass('add-filme-active')
+            }
+        })
     </script>
 @endsection
