8d450ae88752d6d21322fdffbcbf0dc257d2a36e
unknown <elaniobros@gmail.com>
2024-03-06 07:19:30 -0300

    update: auth and hasing

--- app/Http/Controllers/Listagens.php
+++ app/Http/Controllers/Listagens.php
@@ -8,21 +8,26 @@ use Illuminate\Http\Request;
 
 class Listagens extends Controller
 {
-    public function filme()
+    public function filmes()
+    {
+        $ult_filmes = Filmes::select('titulo', 'imdb_code', 'capa_url', 'tipo_capa')->orderBy('created_at', 'DESC')->take(15)->get();
+        return response()->json(Filmes::all());
+    }
+
+    public function filmes_comentarios()
     {
         $filmes_comentarios = Filmes::select('titulo', 'imdb_code', 'capa_url', 'tipo_capa')
             ->withCount('comentarios')
             ->with(['comentarios' => function ($relation) {
                 return $relation->where('grupos_comentario.titulo', '=', 'Comentário Gerais');
             }])->orderBy('comentarios_count', 'DESC')->take(10)->get();
+    }
 
-        $ult_filmes = Filmes::select('titulo', 'imdb_code', 'capa_url', 'tipo_capa')->orderBy('created_at', 'DESC')->take(15)->get();
-
+    public function filmes_categorias()
+    {
         $filmes_categoria = Categorias::with(['filmes' => function ($relation) {
             return $relation->orderBy('created_at', 'DESC');
         }])->orderBy('created_at', 'DESC')->take(5)->get();
-
-        return response()->json(Filmes::all());
     }
 
     public function votados(Request $request)
